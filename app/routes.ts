3b3520bc189bf787e1a896b88513e675cac6a16f
fethulmubin <fetihul@gmail.com>
2025-05-12 23:51:13 +0300

    some error fixation

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,8 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
-    layout("routes/admin/admin-layout.tsx", [
+    layout('routes/admin/admin-layout.tsx', [
         route('dashboard', 'routes/admin/dashboard.tsx'),
-
     ]),
-    
+   
 ] satisfies RouteConfig;
