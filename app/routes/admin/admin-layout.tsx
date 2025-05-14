b2481f0e6b1a99e9e6c3079d2ed8fe2ce8542b61
fethulmubin <fetihul@gmail.com>
2025-05-14 23:01:32 +0300

    side bar for desktop

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,19 +1,24 @@
 // routes/admin/admin-layout.tsx
 // import { Outlet } from "react-router/dev";
 import React from 'react';
+import { SidebarComponent } from '@syncfusion/ej2-react-navigations'
 import { Outlet } from 'react-router';
+import NavItems from 'components/NavItems';
+
 // import { Outlet } from 'react-router';
 
 export default function AdminLayout() {
     return (
         <div className='admin-layout'>
-          mobileSidebar
-          <aside className='w-full max-w-[270px] hidden lg:block'>
-            Sidebar
-          </aside>
-          <aside className='children'>
-            <Outlet/>
-          </aside>
+            mobileSidebar
+            <aside className='w-full max-w-[270px] hidden lg:block'>
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems/>
+                </SidebarComponent>
+            </aside>
+            <aside className='children'>
+                <Outlet />
+            </aside>
         </div>
     );
 }
