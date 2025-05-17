32c318d07959cb2d771a3f8b944f0ab3691a1f96
fethulmubin <fetihul@gmail.com>
2025-05-17 21:49:47 +0300

    stat cards completed with static data

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,3 +1,4 @@
+import { calculateTrendPercentage, cn } from 'lib/utils'
 import React from 'react'
 
 const StatCard = ({
@@ -6,8 +7,32 @@ const StatCard = ({
   lastMonthCount,
   currentMonthCount
 }: StatsCard) => {
+  const { trend, percentage } = calculateTrendPercentage(currentMonthCount,
+    lastMonthCount
+  )
+  const isDecrement = trend === 'decrement';
   return (
-    <div>StatCard</div>
+    <article>
+      <h3 className='text-base font-medium'>
+        {headerTitle}
+      </h3>
+      <div className='content'>
+        <div className="flex flex-col gap4">
+          <h2 className='text-4xl font-semibold'>
+            {total}
+          </h2>
+
+          <div className='flex items-center gap-2' >
+            <figure className='flex items-center gap-1'>
+              <img src={`/assets/icons/${isDecrement ? 'arrow-down-red.svg' : 'arrow-up-green.svg'}`} alt="" />
+              <figcaption className={cn('text-sm font-medium', isDecrement ? 'text-red-500' : 'text-green-500')}>
+                {Math.round(percentage)}%
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </article>
   )
 }
 
