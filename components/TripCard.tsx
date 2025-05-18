c1c2c5d97627e5a35a468a7ff3e0e7ce5ed71b0a
fethulmubin <fetihul@gmail.com>
2025-05-18 08:32:57 +0300

    trip card some editing

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -5,7 +5,7 @@ const TripCard = ({id, name, location, imageUrl, tags, price}:TripCardProps) => 
   const path = useLocation()
   return (
     <Link className='trips-card' to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`} >
-        <img src={imageUrl} alt={name} />
+        <img className='w-50' src={imageUrl} alt={name} />
 
         <article>
           <h2>{name}</h2>
@@ -13,6 +13,11 @@ const TripCard = ({id, name, location, imageUrl, tags, price}:TripCardProps) => 
             <img src='/assets/icons/location-mark.svg' alt="" />
             <figcaption>{location}</figcaption>
           </figure>
+          <div className="flex justify-around">
+            {tags.map((tag, index) =>(
+              <span key={index} className='text-sm font-medium text-gray-100 bg-gray-200/20 rounded-full px-2 py-1'>{tag}</span>
+            ))}
+          </div>
         </article>
     </Link>
   )     
