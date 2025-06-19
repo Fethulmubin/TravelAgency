c73d52ae93c1d75c6365f76e07cf2cfc5158dcbe
fethulmubin <fetihul@gmail.com>
2025-06-19 22:09:53 +0300

    app write keys and some editings

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router'
+import { ChipDirective, ChipListComponent, ChipsDirective } from '@syncfusion/ej2-react-buttons'
+import { cn, getFirstWord } from 'lib/utils'
 
-const TripCard = ({id, name, location, imageUrl, tags, price}:TripCardProps) => {
+const TripCard = ({ id, name, location, imageUrl, tags, price }: TripCardProps) => {
   const path = useLocation()
   return (
-    <Link className='trips-card' to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`} >
-        <img className='w-50' src={imageUrl} alt={name} />
+    <Link className='trip-card' to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`} >
+      <img className='rounded-2xl' src={imageUrl} alt={name} />
 
-        <article>
-          <h2>{name}</h2>
-          <figure>
-            <img src='/assets/icons/location-mark.svg' alt="" />
-            <figcaption>{location}</figcaption>
-          </figure>
-          <div className="flex justify-around">
-            {tags.map((tag, index) =>(
-              <span key={index} className='text-sm font-medium text-gray-100 bg-gray-200/20 rounded-full px-2 py-1'>{tag}</span>
+      <article>
+        <h2>{name}</h2>
+        <figure>
+          <img className='size-4' src='/assets/icons/location-mark.svg' alt="" />
+          <figcaption>{location}</figcaption>
+        </figure>
+      </article>
+      <div className="mt-5 pr-3.5 pb-5 pl-[18px]">
+
+        <ChipListComponent id='travel-chip '>
+          <ChipsDirective>
+            {tags.map((tag, index) => (
+              <ChipDirective
+                key={index}
+                text={getFirstWord(tag)}
+                cssClass={cn(index === 1 ? '!bg-pink-50 !text-pink-500' : 'bg-success-50 !text-success-700')}
+              />
             ))}
-          </div>
-        </article>
+          </ChipsDirective>
+        </ChipListComponent>
+      </div>
+      <article className='tripCard-pill'>{price}</article>
     </Link>
-  )     
+  )
 }
 
 export default TripCard
